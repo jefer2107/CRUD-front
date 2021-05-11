71bc34f9301c53624cac730842538ca3f579ebfc
Jefferson Dias <54519758+jefer2107@users.noreply.github.com>
2021-05-11 13:19:44 -0300

    CRUD.1.1

--- src/components/header.js
+++ src/components/header.js
@@ -9,6 +9,7 @@ export default function Header(){
             <ol>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/register-user">Cadastro de usuário</Link></li>
+                <li><Link to="/user-list">Lista de usuários</Link></li>
             </ol>
         </header>
     )
--- src/pages/users/list/listUser.js
+++ src/pages/users/list/listUser.js
@@ -1,13 +1,25 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./listUser.css"
 
 export default function ListUser(){
 
-    axios.get('http://localhost:4000/users')
+    const [user,setUser] = useState([])
+
+    useEffect(()=>{
+        axios.get('http://localhost:4000/users')
         .then((x)=>{
-            x.data
-        })
+            setUser(x.data)
+            console.log(x.data)
+        }) 
+    },[])
+
+    const onCancel = (id)=>{
+        axios.delete(`http://localhost:4000/users/${id}`,id)
+            .then((x)=>{
+                console.log(x.data)
+            })
+    }
 
     return(
 
@@ -15,14 +27,27 @@ export default function ListUser(){
             <table className="container">
                 <thead className="text-center font-weight-bold">
                     <tr>
+                        <td>ID</td>
                         <td>NOME</td>
                         <td>EMAIL</td>
                         <td>EDIÇAO</td>
                     </tr>
-                    <tbody>
-                        
-                    </tbody>
                 </thead>
+                <tbody className="text-center">
+                    {user.map((x)=>{
+                        return(
+                            <tr>
+                                <td> {x._id} </td>
+                                <td> {x.nome} </td>
+                                <td> {x.email} </td>
+                                <td>
+                                    <button type="button" onCancel={()=>onCancel(x._id)}  className="btn btn-primary btn-xs btn-block">Editar</button>
+                                    <button className="btn btn-danger btn-xs btn-block">Cancelar</button>
+                                </td>
+                            </tr>
+                        )
+                    })}             
+                    </tbody>
             </table>
         </div>
     )
